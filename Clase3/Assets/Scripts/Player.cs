858f174cbbf3c8170ef822dfdb2ca507c6faec26
Erick Marín <erick.marin.v@gmail.com>
2019-07-21 15:15:15 -0600

    Clase 3

---
+++ Clase3/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    [SerializeField]
+    private float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody2D = transform.GetComponent<Rigidbody2D>();
+        Debug.Log("Metodo de inicio");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate(){
+        movement();
+    }
+
+    public void movement(){
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(this.speed * Input.GetAxis("Horizontal"),0);
+    }
+}
